f95699f24ef5f71cb64b39b69992eb5e3d2aaaca
Julian De Angelis <julian.deangelis@gmail.com>
2022-09-05 12:06:38 -0400

    Create UI
    - No button generates new phone number

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
 
+const minNumber = 1000000000
+const maxNumber = 9999999999
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [phoneNumber, setPhoneNumber] = useState(0)
+
+    useEffect(() => {
+
+        RandomPhoneNumber()
+
+    }, [])
+
+    const RandomPhoneNumber = () => {
+
+        var number = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber)
+        setPhoneNumber(number)
+
+    }
+
+    return (
+        <div className="App">
+            <div className='App-header'>
+                <div>
+                    Is this your phone number? {phoneNumber}
+                </div>
+                <div>
+                    <button>yes</button>
+                    {' '}
+                    <button onClick={() => RandomPhoneNumber()}>no</button>
+                </div>
+            </div>
+        </div>
+
+    );
 }
 
 export default App;
