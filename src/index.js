1bb0cfcc8341a95ffbdb1f8f4138be2b0549af94
Julian De Angelis <julian.deangelis@gmail.com>
2022-09-07 11:20:03 -0400

    Add firebase to project
    - Include analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import IsThisYourNumber from './IsThisYourNumber';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +17,19 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCMEXLQLdrwwyLHM5wC4Pyhf2KD32E5LWU",
+    authDomain: "isthisyournumber-842a8.firebaseapp.com",
+    projectId: "isthisyournumber-842a8",
+    storageBucket: "isthisyournumber-842a8.appspot.com",
+    messagingSenderId: "1069589150294",
+    appId: "1:1069589150294:web:a619e55d50bb9a929899f7",
+    measurementId: "G-VX0KN962C0"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
